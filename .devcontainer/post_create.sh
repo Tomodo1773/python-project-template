d12da33c73a89e915b42f4989c8fa119e842956b
Tomodo1773 <forever-career@hotmail.co.jp>
2025-10-21 01:28:48 +0000

    feat(post_create): devcontainersとcodespacesの設定を追加 devcontainersとcodespacesのテーマ設定を追加しました。

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 
+git config --global devcontainers-theme.hide-status 1
+git config --global codespaces-theme.hide-status 1
+
 uv sync || true
 uv run pre-commit install || true
-
